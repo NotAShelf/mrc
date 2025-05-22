cc07bd09081fbfd2a8cd11a27790a468d186ed64
NotAShelf <NotAShelf@users.noreply.github.com>
2025-05-22 18:44:44 +0000

    deploy: 0ec716f262801548b15ed16a36b9ebf5aff952c4

--- trait.impl/core/fmt/trait.Debug.js
+++ trait.impl/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["mrc",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"mrc/enum.MpvCommand.html\" title=\"enum mrc::MpvCommand\">MpvCommand</a>"]]]]);
+    var implementors = Object.fromEntries([["mrc",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"mrc/enum.MpvCommand.html\" title=\"enum mrc::MpvCommand\">MpvCommand</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {

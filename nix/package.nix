eb2dc61c70748f2bbccd75efadc0673d79ae1956
NotAShelf <raf@notashelf.dev>
2025-06-11 18:20:02 +0300

    nix: filter source

--- nix/package.nix
+++ nix/package.nix
@@ -1,20 +1,28 @@
 {
   lib,
   rustPlatform,
-}:
-rustPlatform.buildRustPackage (finalAttrs: {
-  pname = "mrc";
-  version = "0.1.0";
-  src = builtins.path {
-    path = ../.;
-    name = "mrc";
-  };
+}: let
+  fs = lib.fileset;
+  s = ../.;
+in
+  rustPlatform.buildRustPackage (finalAttrs: {
+    pname = "mrc";
+    version = "0.1.0";
+    src = fs.toSource {
+      root = s;
+      fileset = fs.unions [
+        (fs.fileFilter (file: builtins.any file.hasExt ["rs"]) (s + /src))
+        (s + /Cargo.lock)
+        (s + /Cargo.toml)
+      ];
+    };
 
-  cargoLock.lockFile = finalAttrs.src + /Cargo.lock;
+    cargoLock.lockFile = finalAttrs.src + /Cargo.lock;
 
-  meta = {
-    description = "IPC wrapper & command-line controller for MPV, the video player ";
-    mainProgram = "mrc";
-    license = lib.licenses.mpl20;
-  };
-})
+    meta = {
+      description = "IPC wrapper & command-line controller for MPV, the video player ";
+      mainProgram = "mrc";
+      license = lib.licenses.mpl20;
+      maintainers = [lib.maintainers.NotAShelf];
+    };
+  })
